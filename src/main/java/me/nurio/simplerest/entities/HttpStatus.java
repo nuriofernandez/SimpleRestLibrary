34dc6c73d7c9834f40e37c59aa863b089548ad75
Nurio Fernández <personal@nurio.me>
2021-01-13 06:57:19 +0100

    Implemented most common HTTP Status codes

--- src/main/java/me/nurio/simplerest/entities/HttpStatus.java
+++ src/main/java/me/nurio/simplerest/entities/HttpStatus.java
@@ -7,7 +7,16 @@ import lombok.Getter;
 @AllArgsConstructor
 public enum HttpStatus {
 
-    OK(200, "OK");
+    OK(200, "OK"),
+    CREATED(201, "Created"),
+    ACCEPTED(202, "Accepted"),
+    BAD_REQUEST(400, "Bad Request"),
+    UNAUTHORIZED(401, "Unauthorized"),
+    FORBIDDEN(403, "Forbidden"),
+    NOT_FOUND(404, "Not Found"),
+    METHOD_NOT_ALLOWED(405, "Method Not Allowed"),
+    INTERNAL_SERVER_ERROR(500, "Internal Server Error"),
+    BAD_GATEWAY(502, "Bad Gateway");
 
     private int code;
     private String description;
