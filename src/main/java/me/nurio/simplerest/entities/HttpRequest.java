1d8306e693b607111b9fe7b51fcd48e532faec4b
Nurio Fernández <personal@nurio.me>
2021-01-16 05:52:53 +0100

    Implemented HttpHeadersManagement to HttpRequest

--- src/main/java/me/nurio/simplerest/entities/HttpRequest.java
+++ src/main/java/me/nurio/simplerest/entities/HttpRequest.java
@@ -9,23 +9,44 @@ import java.util.StringTokenizer;
 
 @Getter
 @RequiredArgsConstructor
-public class HttpRequest {
+public class HttpRequest extends HttpHeadersManagement {
 
     private final String requestId;
-    private final InetAddress remoteAddress;
+    private InetAddress remoteAddress;
 
-    private final String method;
-    private final String path;
-    private final String protocol;
-
-    private final Map<String, String> headers;
+    private String method;
+    private String path;
+    private String protocol;
 
     public HttpRequest(String requestId, InetAddress remoteAddress, String startLine, Map<String, String> headers) {
         this.remoteAddress = remoteAddress;
         this.requestId = requestId;
-        this.headers = headers;
 
-        // Parse the header
+        // Parse raw http contents.
+        parseHeaders(headers);
+        parseStartLine(startLine);
+    }
+
+    /**
+     * Parses raw headers to use HttpHeadersManagement.
+     *
+     * @param headers Map of raw key-value headers.
+     */
+    private void parseHeaders(Map<String, String> headers) {
+        for (Map.Entry<String, String> header : headers.entrySet()) {
+            String key = header.getKey();
+            for (String value : header.getValue().split(",")) {
+                addHeader(key, value);
+            }
+        }
+    }
+
+    /**
+     * Parses raw http start line.
+     *
+     * @param startLine Http raw start line.
+     */
+    private void parseStartLine(String startLine) {
         StringTokenizer parse = new StringTokenizer(startLine);
         method = parse.nextToken().toUpperCase();
         path = parse.nextToken().toLowerCase();
